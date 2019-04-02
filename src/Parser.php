4cd4ac3d27c772d0c94f76e158a5a399242713ec
Sectrum <s3ctrum@yandex.ru>
2019-04-03 00:21:09 +0500

    Big changes

--- src/Parser.php
+++ src/Parser.php
@@ -5,7 +5,7 @@ namespace s3ctrum\parser;
 
 class Parser implements ParserInterface
 {
-    public function process(string $url, string $tag): array
+    public function process(string $tag, string $url): array
     {
         $htmlPage = file_get_contents($url);
 
